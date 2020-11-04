0824abac450371eefb4bd90fb56ff4941b850708
goofara <73748363+goofara@users.noreply.github.com>
2020-11-04 19:51:13 +0200

    Update file list for macos

--- premake5.lua
+++ premake5.lua
@@ -8,22 +8,46 @@ project "GLFW"
     files
     {
       "include/GLFW/glfw3.h",
-		  "include/GLFW/glfw3native.h",
-		  "src/glfw_config.h",
-		  "src/context.c",
-		  "src/init.c",
+      "include/GLFW/glfw3native.h",
+      "src/glfw_config.h",
+      "src/context.c",
+      "src/init.c",
       "src/input.c",
       "src/monitor.c",
       "src/vulkan.c",
       "src/window.c"
     }
     
+    filter "system:macosx"
+        cppdialect "C++11"
+        staticruntime "On"
+        systemversion "10.15"
+        
+        files
+        {
+            "src/cocoa_init.m",
+            "src/cocoa_joystick.m",
+            "src/cocoa_monitor.m",
+            "src/cocoa_time.c",
+            "src/cocoa_window.m",
+            "src/posix_thread.c",
+            "src/nsgl_context.m",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
+        }
+
+        defines
+        {
+            "_GLFW_COCOA",
+            "_CRT_SECURE_NO_WARNINGS"
+        }
+
     filter "system:windows"
 		  systemversion "latest"
 		  staticruntime "On"
       
-      files
-      {
+    files
+    {
         "src/win32_init.c",
         "src/win32_joystick.c",
         "src/win32_monitor.c",
@@ -33,7 +57,7 @@ project "GLFW"
         "src/wgl_context.c",
         "src/egl_context.c",
         "src/osmesa_context.c"
-      }
+    }
 
       defines 
       { 
@@ -45,6 +69,6 @@ project "GLFW"
 		  runtime "Debug"
 		  symbols "on"
 
-	  filter "configurations:Release"
+    filter "configurations:Release"
 		  runtime "Release"
 		  optimize "on"
